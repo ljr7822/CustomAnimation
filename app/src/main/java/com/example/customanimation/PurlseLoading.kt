1554619891f9eddd3201aad6673355851a862357
iwenLjr <1816807688@qq.com>
2020-09-20 22:30:55 +0800

    小球跳动动画加载完成

--- app/src/main/java/com/example/customanimation/PurlseLoading.kt
+++ app/src/main/java/com/example/customanimation/PurlseLoading.kt
@@ -69,6 +69,9 @@ class PurlseLoading : View {
 
     private var animators = AnimatorSet()
 
+    /**
+     * 创建圆
+     */
     private fun createAnim() {
 
         val lists = mutableListOf<ValueAnimator>()
